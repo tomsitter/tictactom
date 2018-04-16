4aa169b5a6b51027833e139f0610811447730610
Tom <thomas.sitter@gmail.com>
2018-04-16 19:20:14 -0400

    Add move, AI move, and board updating

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -9,7 +9,7 @@ import { NavToggleButton } from '../styled/NavDrawer'
 
 class NavDrawer extends Component {
     state = {
-        open: true,
+        open: false,
         width: 250
     }
 
--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import {Stage} from 'react-konva'
-import {Board} from '../styled/TicTacToe'
+import {Board, Squares} from '../styled/TicTacToe'
 
 class TicTacToe extends Component {
 
+    constructor(props) {
+        super(props)
+        this.combos = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+    }
+
     state = {
         rows: 3,
-        // gameState: newArray(9).fill(false),
-        // ownMark: 'X',
-        // otherMark: 'O',
-        // gameOver: false,
-        // yourTurn: true,
-        // winner: false,
+        gameState: new Array(9).fill(false),
+        ownMark: 'X',
+        otherMark: 'O',
+        gameOver: false,
+        yourTurn: true,
+        win: [],
     }
 
     componentWillMount() {
@@ -20,20 +34,66 @@ class TicTacToe extends Component {
         let size = (height < width) ? height * 0.8 : width * 0.8
         let rows = this.state.rows
         let unit = size / rows
+        let coordinates = []
+        for (let y=0; y<rows; y++) {
+            for (let x=0; x<rows; x++) {
+                coordinates.push([x*unit, y*unit])
+            }
+        }
 
         this.setState({
+            unit,
             size,
             rows,
-            unit
+            coordinates
         })
     }
     
-    makePlayerMove = () => {
-        // player moves
+    move = (index, marker) => {
+        console.log('Player placed', marker, 'at', index)
+        let newGameState = this.state.gameState;
+        newGameState[index] = marker;
+        let win = this.winChecker(newGameState)
+        let gameOver = win ? true : false
+        if (gameOver) {
+            this.state.yourTurn ? console.log("You won!") : console.log("You lost!")
+        }
+        this.setState((prevState, props) => {
+            return {
+                gameState: newGameState,
+                yourTurn: !prevState.yourTurn,
+                win: win,
+                gameOver: gameOver
+            }
+        })
+        
+        if (!gameOver && !this.state.yourTurn) this.makeAiMove(newGameState)
     }
 
-    makeAiMove = () => {
-        // ai moves
+    makeAiMove = (gameState) => {
+        let otherMark = this.state.otherMark
+        let openSquares = []
+        gameState.forEach( (square, index) => {
+            if (!square) {
+                openSquares.push(index)
+            }
+        })
+        let aiMove = openSquares[this.random(0, openSquares.length)]
+        this.move(aiMove, otherMark)
+    }
+
+    random = (min, max) => {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max-min)) + min
+    }
+
+    winChecker = (gameState) => {
+        let combos = this.combos
+        return combos.find( (combo) => {
+            let [a,b,c] = combo
+            return (gameState[a] === gameState[b] && gameState[a] === gameState[c] && gameState[a] !== false)
+        })
     }
 
     turingTest = () => {
@@ -48,7 +108,13 @@ class TicTacToe extends Component {
         let {
             unit,
             size,
-            rows
+            rows,
+            coordinates,
+            gameState,
+            win,
+            gameOver,
+            yourTurn,
+            ownMark
         } = this.state
 
         return (
@@ -62,7 +128,16 @@ class TicTacToe extends Component {
                         size={size}
                         rows={rows}
                     />
-                    {/* <Squares/> */}
+                    <Squares
+                        unit={unit}
+                        coordinates={coordinates}
+                        gameState={gameState}
+                        gameOver={gameOver}
+                        ownMark={ownMark}
+                        yourTurn={yourTurn}
+                        win={win}
+                        move={this.move}
+                    />
                 </Stage>
             </div>
         )
